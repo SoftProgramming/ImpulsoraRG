d25661fb380a3ca1488d5cc7cedc987c67075981
210PC <scopty210@gmail.com>
2018-01-23 11:35:28 -0600

    Agregar .gitignore y .gitattributes.

---
+++ ImpulsoraRG/ImpulsoraRG/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImpulsoraRG
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var url = "http://www.impulsorarg.com/";
+            vizualizadorWeb.Source = url;
+            vizualizadorWeb.HeightRequest = 1;
+
+
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            if (vizualizadorWeb.CanGoBack)
+            {
+                vizualizadorWeb.GoBack();
+                return true;
+            }
+            else
+            {
+                base.OnBackButtonPressed();
+                return false;
+            }
+        }
+    }
+}
